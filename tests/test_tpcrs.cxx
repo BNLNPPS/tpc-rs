b7fc9302fad6695991545f5375c69de1f6808255
Dmitri Smirnov <dmixsmi@gmail.com>
2020-02-12 16:31:20 -0500

    [tests] Read tpcrs input values from ROOT file

--- tests/test_tpcrs.cxx
+++ tests/test_tpcrs.cxx
@@ -1,6 +1,35 @@
+#include <iostream>
+
+#include "TChain.h"
+
 #include "StTpcRSMaker/StTpcRSMaker.h"
+#include "tests/GeantEvent.h"
+
 
 int main(int argc, char **argv)
 {
-   return EXIT_SUCCESS;
+  StTpcRSMaker* tpcrs = new StTpcRSMaker();
+
+  TChain trsTreeChain("t", "tpcrs test TTree");
+  trsTreeChain.AddFile("geant_event.root");
+
+  GeantEvent* geantEvent = new GeantEvent();
+
+  trsTreeChain.SetBranchAddress("b", &geantEvent);
+
+  const int nRecords = trsTreeChain.GetEntries();
+
+  std::cout << "nRecords: " << nRecords << std::endl;
+
+  std::ofstream logFile("test_tpcrs.log");
+
+  for (int iRecord = 1; iRecord <= nRecords; iRecord++)
+  {
+    trsTreeChain.GetEntry(iRecord - 1);
+    geantEvent->Print(logFile);
+  }
+
+  delete geantEvent;
+
+  return EXIT_SUCCESS;
 }
