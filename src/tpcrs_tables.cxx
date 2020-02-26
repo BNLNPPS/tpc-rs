f22240b436938fe518bea7520a7fec24b03b2c4a
Dmitri Smirnov <dmixsmi@gmail.com>
2020-02-25 19:14:11 -0500

    [cmake] Add table dictionaries to the library

---
+++ src/tpcrs_tables.cxx
@@ -0,0 +1,103 @@
+#include "Ttypes.h"
+
+#include "tables/St_asic_thresholds_Table.h"
+#include "tables/St_g2t_tpc_hit_Table.h"
+#include "tables/St_g2t_track_Table.h"
+#include "tables/St_g2t_vertex_Table.h"
+#include "tables/St_itpcPadGainT0_Table.h"
+#include "tables/St_iTPCSurvey_Table.h"
+#include "tables/St_MagFactor_Table.h"
+#include "tables/St_richvoltages_Table.h"
+#include "tables/St_spaceChargeCor_Table.h"
+#include "tables/St_starClockOnl_Table.h"
+#include "tables/St_starMagOnl_Table.h"
+#include "tables/St_Survey_Table.h"
+#include "tables/St_tpcAltroParams_Table.h"
+#include "tables/St_tpcAnodeHV_Table.h"
+#include "tables/St_tpcAnodeHVavg_Table.h"
+#include "tables/St_TpcAvgCurrent_Table.h"
+#include "tables/St_TpcAvgPowerSupply_Table.h"
+#include "tables/St_tpcCalibResolutions_Table.h"
+#include "tables/St_tpcChargeEvent_Table.h"
+#include "tables/St_tpcCorrection_Table.h"
+#include "tables/St_tpcDimensions_Table.h"
+#include "tables/St_tpcDriftVelocity_Table.h"
+#include "tables/St_tpcEffectiveGeom_Table.h"
+#include "tables/St_tpcElectronics_Table.h"
+#include "tables/St_tpcFieldCage_Table.h"
+#include "tables/St_tpcFieldCageShort_Table.h"
+#include "tables/St_tpcGas_Table.h"
+#include "tables/St_tpcGlobalPosition_Table.h"
+#include "tables/St_tpcGridLeak_Table.h"
+#include "tables/St_tpcHighVoltages_Table.h"
+#include "tables/St_tpcHVPlanes_Table.h"
+#include "tables/St_tpcOmegaTau_Table.h"
+#include "tables/St_tpcPadConfig_Table.h"
+#include "tables/St_tpcPadGainT0_Table.h"
+#include "tables/St_tpcPadPlanes_Table.h"
+#include "tables/St_tpcPadResponse_Table.h"
+#include "tables/St_tpcPadrowT0_Table.h"
+#include "tables/St_tpcPedestal_Table.h"
+#include "tables/St_tpcRDOMap_Table.h"
+#include "tables/St_tpcRDOMasks_Table.h"
+#include "tables/St_tpcRDOT0offset_Table.h"
+#include "tables/St_TpcResponseSimulator_Table.h"
+#include "tables/St_tpcSCGL_Table.h"
+#include "tables/St_tpcSectorPosition_Table.h"
+#include "tables/St_tpcSectorT0offset_Table.h"
+#include "tables/St_tpcSlowControlSim_Table.h"
+#include "tables/St_tpcWirePlanes_Table.h"
+#include "tables/St_trgTimeOffset_Table.h"
+#include "tables/St_trigDetSums_Table.h"
+#include "tables/St_tss_tsspar_Table.h"
+
+TableImpl(asic_thresholds)
+TableImpl(g2t_tpc_hit)
+TableImpl(g2t_track)
+TableImpl(g2t_vertex)
+TableImpl(itpcPadGainT0)
+TableImpl(iTPCSurvey)
+TableImpl(MagFactor)
+TableImpl(richvoltages)
+TableImpl(spaceChargeCor)
+TableImpl(starClockOnl)
+TableImpl(starMagOnl)
+TableImpl(Survey)
+TableImpl(tpcAltroParams)
+TableImpl(tpcAnodeHV)
+TableImpl(tpcAnodeHVavg)
+TableImpl(TpcAvgCurrent)
+TableImpl(TpcAvgPowerSupply)
+TableImpl(tpcCalibResolutions)
+TableImpl(tpcChargeEvent)
+TableImpl(tpcCorrection)
+TableImpl(tpcDimensions)
+TableImpl(tpcDriftVelocity)
+TableImpl(tpcEffectiveGeom)
+TableImpl(tpcElectronics)
+TableImpl(tpcFieldCage)
+TableImpl(tpcFieldCageShort)
+TableImpl(tpcGas)
+TableImpl(tpcGlobalPosition)
+TableImpl(tpcGridLeak)
+TableImpl(tpcHighVoltages)
+TableImpl(tpcHVPlanes)
+TableImpl(tpcOmegaTau)
+TableImpl(tpcPadConfig)
+TableImpl(tpcPadGainT0)
+TableImpl(tpcPadPlanes)
+TableImpl(tpcPadResponse)
+TableImpl(tpcPadrowT0)
+TableImpl(tpcPedestal)
+TableImpl(tpcRDOMap)
+TableImpl(tpcRDOMasks)
+TableImpl(tpcRDOT0offset)
+TableImpl(TpcResponseSimulator)
+TableImpl(tpcSCGL)
+TableImpl(tpcSectorPosition)
+TableImpl(tpcSectorT0offset)
+TableImpl(tpcSlowControlSim)
+TableImpl(tpcWirePlanes)
+TableImpl(trgTimeOffset)
+TableImpl(trigDetSums)
+TableImpl(tss_tsspar)
