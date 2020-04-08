30494854caa5a1b4b5e017617b6af3c10983f3a4
Dmitri Smirnov <dmixsmi@gmail.com>
2020-04-08 17:38:37 -0400

    Remove unneeded files

--- St_base/StMessMgr.cxx
+++
@@ -1,9 +0,0 @@
-#include "St_base/StMessMgr.h"
-
-std::ostream& LOG_INFO = std::cout;
-std::ostream& LOG_WARN = std::cout;
-std::ostream& LOG_ERROR = std::cerr;
-std::ostream& LOG_FATAL = std::cerr;
-std::ostream& LOG_DEBUG = std::cout;
-
-std::ostream& endm(std::ostream& os) { return std::endl(os); }
--- src/tpcrs_tables.cxx
+++
@@ -1,9 +0,0 @@
-#include "Ttypes.h"
-
-#include "tables/St_g2t_tpc_hit_Table.h"
-#include "tables/St_g2t_track_Table.h"
-#include "tables/St_g2t_vertex_Table.h"
-
-TableImpl(g2t_tpc_hit)
-TableImpl(g2t_track)
-TableImpl(g2t_vertex)
