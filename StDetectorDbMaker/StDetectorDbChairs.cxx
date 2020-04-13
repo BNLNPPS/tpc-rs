57b47db22fd323e494dd8f7e95807290c748443d
Dmitri Smirnov <dmixsmi@gmail.com>
2020-04-13 17:47:28 -0400

    [cstruct] Remove recursive Struct() not compatible with ConfigStruct

--- StDetectorDbMaker/StDetectorDbChairs.cxx
+++ StDetectorDbMaker/StDetectorDbChairs.cxx
@@ -954,15 +954,6 @@ MakeChairInstance(MagFactor,RunLog/MagFactor);
 #include "StDetectorDbMaker/St_starClockOnlC.h"
 MakeChairInstance(starClockOnl,RunLog/onl/starClockOnl);
 
-
-starClockOnl_st *St_starClockOnlC::Struct(Int_t i) {
-  starClockOnl_st *s = instance()->GetTable();
-  Int_t N =  GetNRows(); // with i < 0 look for positive frequency
-  if (i >= 0 && i < N) return s + i;
-  for (Int_t j = 0; j < N; j++, s++) if (s->frequency > 0) break;
-  assert(s->frequency > 0 && s->frequency < 1e7);
-  return s;
-}
 #include "StDetectorDbMaker/St_starMagOnlC.h"
 MakeChairInstance(starMagOnl,RunLog/onl/starMagOnl);
 #include "St_beamInfoC.h"
