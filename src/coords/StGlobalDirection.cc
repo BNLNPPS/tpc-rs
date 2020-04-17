2ea1425e3baac06da9bcb7c809969c90b6667db5
Dmitri Smirnov <dmixsmi@gmail.com>
2020-04-17 13:36:37 -0400

    Remove deprecated files

--- src/coords/StGlobalDirection.cc
+++
@@ -1,8 +0,0 @@
-#include "coords/StGlobalDirection.hh"
-std::ostream &operator<<(std::ostream &os, const StGlobalDirection &a)
-{
-  return os << "GC direction ( "
-         << a.position().x() << ", "
-         << a.position().y() << ", "
-         << a.position().z() << ")";
-}
