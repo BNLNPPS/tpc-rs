67b141788bd3b31c3f6f17103768ae7a59d82037
Dmitri Smirnov <dmixsmi@gmail.com>
2020-04-27 21:10:20 -0400

    [cmake] Setup external yaml-cpp in separate cmake file

---
+++ cmake/ExternalProject_yaml-cpp.cmake
@@ -0,0 +1,17 @@
+include(ExternalProject)
+
+# yaml-cpp
+ExternalProject_Add(
+	yaml-cpp
+	URL        https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.3.tar.gz
+	URL_HASH   MD5=b45bf1089a382e81f6b661062c10d0c2
+	PREFIX     ${CMAKE_BINARY_DIR}/contrib/
+	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+	           -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=ON
+)
+
+set(YAML_CPP_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/contrib/)
+
+add_library(yaml-cpp-lib STATIC IMPORTED)
+set_property(TARGET yaml-cpp-lib PROPERTY IMPORTED_LOCATION ${YAML_CPP_INSTALL_PREFIX}/lib/libyaml-cpp.a)
+add_dependencies(yaml-cpp-lib yaml-cpp)
