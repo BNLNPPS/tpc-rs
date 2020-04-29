82d348f928411c07388b85f7b63eac6813d45957
Dmitri Smirnov <dmixsmi@gmail.com>
2020-04-29 11:07:50 -0400

    [test] Generate ROOT dictionaries for geant structs

--- tests/LinkDef.h
+++ tests/LinkDef.h
@@ -4,6 +4,9 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ struct g2t_tpc_hit_st+;
+#pragma link C++ struct g2t_track_st+;
+#pragma link C++ struct g2t_vertex_st+;
 #pragma link C++ struct DigitizedHit+;
 #pragma link C++ struct GeantEvent+;
 
