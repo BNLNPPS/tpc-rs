bf7a17069cb7c0476e367b5295db74a086dbe263
Dmitri Smirnov <dmixsmi@gmail.com>
2020-05-18 23:29:31 -0400

    Add new public interface prototypes and implementation

--- include/tpcrs/tpcrs.h
+++ include/tpcrs/tpcrs.h
@@ -3,26 +3,9 @@
 
 #include <vector>
 
+#include "tpcrs/configurator.h"
+
 namespace tpcrs {
-
-struct GeantHit
-{
-  int track_id;   /* Parent track */
-  int particle_id;    /* GEANT particle id */
-  int volume_id; /* Volume id packed as SSRR, SS = sector, RR = pad row */
-  float x[3];    /* Hit center */
-  float p[3];    /* Local momentum */
-  float de;      /* energy deposited at hit */
-  float ds;      /* path length within pad row */
-  float len;     /* track length up to this hit */
-  double tof;     /// Time of flight including the GEANT vertex production time
-  float lgam;    /* ALOG10(GEKin/AMass) */
-  float adc;     /* OUTPUT signal in ADC  after digitization */
-  float pad;     /* OUTPUT hit pad position used in digitization */
-  float timebin; /* OUTPUT hit time position -"- */
-  int np;        /* OUTPUT no. of primary electrons */
-};
-
 
 /**
  * Associates a packed hardware index with the data.
@@ -37,6 +20,34 @@ struct DigiChannel
   unsigned int sector : 5, row : 7, pad : 10, timebin : 10;
   short adc;
   short idt;
+};
+
+
+struct SimuHit
+{
+  int np;         /* no. of primary electrons */
+  double de;      /* energy deposited at hit */
+  double ds;      /* path length within pad row */
+  float adc;
+  float pad;
+  float timebin;
+  //DigiChannel ch;
+};
+
+
+struct GeantHit
+{
+  int track_id;   /* Parent track */
+  int particle_id;    /* GEANT particle id */
+  int volume_id; /* Volume id packed as SSRR, SS = sector, RR = pad row */
+  float x[3];    /* Hit center */
+  float p[3];    /* Local momentum */
+  float de;      /* energy deposited at hit */
+  float ds;      /* path length within pad row */
+  float len;     /* track length up to this hit */
+  double tof;     /// Time of flight including the GEANT vertex production time
+  float lgam;     /// Deprecated. ALOG10(GEKin/AMass)
+  SimuHit digi;   /// Deprecated.
 };
 
 
@@ -67,6 +78,20 @@ class DigiData
 
   std::vector<DigiChannel> channels_;
 };
+
+
+template<typename Simulator, typename InputIt, typename OutputIt>
+OutputIt simulate(InputIt first1, InputIt last1, OutputIt d_first, const Configurator& cfg)
+{
+  static Simulator simu(cfg);
+
+  std::vector<GeantHit> hits(first1, last1);
+  DigiData digi_data;
+
+  simu.Simulate(hits, digi_data);
+
+  return std::copy(std::begin(digi_data.channels()), std::end(digi_data.channels()), d_first);
+}
 
 }
 
